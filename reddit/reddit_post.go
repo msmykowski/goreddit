a4f1340ba97655672917ee1cf7bcb9d037d6f4ab
msmykowski <smycal@gmail.com>
2015-10-06 21:43:12 -0700

    refactor #GetImgurId

--- reddit/reddit_post.go
+++ reddit/reddit_post.go
@@ -10,15 +10,17 @@ type RedditPost struct {
 	Score int
 }
 
-func (r RedditPost) GetImgurId() string {
+func (r RedditPost) GetImgurId() (i string) {
 	splitURL := strings.Split(r.URL, "/")
-	imgurId := splitURL[3]
+	indicator := splitURL[3]
 
-	if imgurId == "a" || imgurId == "g" || imgurId == "gallery" {
-		imgurId = splitURL[4]
-		return imgurId
+	if indicator == "a" || indicator == "g" || indicator == "gallery" {
+		i = splitURL[4]
+	} else if strings.Contains(indicator, ".") {
+		i = strings.Split(indicator, ".")[0]
 	} else {
-		imgurId = strings.Split(imgurId, ".")[0]
-		return imgurId
+		i = indicator
 	}
+
+	return i
 }
